db2740be2d719cd69de37c16559dcbdfadfa6648
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-12 18:31:12 +0100

    feat: initial release

--- __tests__/main-test.ts
+++ __tests__/main-test.ts
@@ -1,1 +1,24 @@
-it('', () => { expect(true).toEqual(true); });
+import path from 'path';
+import rdfServe from 'rdf-serve';
+import dereference from '../lib';
+
+it('should fetch a local document', async () => {
+  const { store, prefixes } = await dereference(path.join(__dirname, 'data', 'test.ttl'), { localFiles: true });
+  expect(store.size).toBe(3);
+  expect(prefixes).toEqual({
+    ex: 'http://example.org/',
+  });
+});
+
+it('should fetch a remote document', async () => {
+  const app = rdfServe(path.join(__dirname, 'data'));
+  const server = app.listen(3005);
+  await new Promise((resolve) => { server.on('listening', resolve); });
+
+  const { store, prefixes } = await dereference('http://localhost:3005/test');
+  expect(store.size).toBe(3);
+  // Note no prefixes because the serve doesnt sent them
+  expect(prefixes).toEqual({});
+
+  server.close();
+});
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,14 @@
-export default {};
+import { Store } from 'n3';
+import dereference from 'rdf-dereference';
+import { promisifyEventEmitter } from 'event-emitter-promisify';
+import { DatasetCore } from '@rdfjs/types';
+
+export default async function dereferenceToStore(
+  ...args: Awaited<Parameters<typeof dereference.dereference>>
+): Promise<{ store: DatasetCore, prefixes: Record<string, string> }> {
+  const store = new Store();
+  const prefixes: Record<string, string> = {};
+  const { data } = (await dereference.dereference(...args));
+  data.on('prefix', (prefix, ns) => { prefixes[prefix] = typeof ns === 'string' ? ns : ns.value; });
+  return { store: await promisifyEventEmitter(store.import(data), store), prefixes };
+}
