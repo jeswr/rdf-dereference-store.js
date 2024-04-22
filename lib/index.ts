8c18206d11a55212af8ba2673fae137e80d3d2ea
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-22 14:46:51 +0100

    feat: support parsing

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,26 @@
 import { Store } from 'n3';
 import dereference from 'rdf-dereference';
+import parseStream from 'rdf-parse';
 import { promisifyEventEmitter } from 'event-emitter-promisify';
-import { DatasetCore } from '@rdfjs/types';
+import { DatasetCore, Stream, Quad } from '@rdfjs/types';
+
+export interface StoreAndPrefixes {
+  store: DatasetCore,
+  prefixes: Record<string, string>
+}
+
+export function streamToStore(data: Stream<Quad>) {
+  const res = { store: new Store(), prefixes: {} as Record<string, string> };
+  data.on('prefix', (prefix, ns) => { res.prefixes[prefix] = typeof ns === 'string' ? ns : ns.value; });
+  return promisifyEventEmitter(res.store.import(data), res);
+}
 
 export default async function dereferenceToStore(
   ...args: Awaited<Parameters<typeof dereference.dereference>>
-): Promise<{ store: DatasetCore, prefixes: Record<string, string> }> {
-  const store = new Store();
-  const prefixes: Record<string, string> = {};
-  const { data } = (await dereference.dereference(...args));
-  data.on('prefix', (prefix, ns) => { prefixes[prefix] = typeof ns === 'string' ? ns : ns.value; });
-  return { store: await promisifyEventEmitter(store.import(data), store), prefixes };
+) {
+  return streamToStore((await dereference.dereference(...args)).data);
+}
+
+export async function parse(...args: Awaited<Parameters<typeof parseStream.parse>>) {
+  return streamToStore(parseStream.parse(...args));
 }
